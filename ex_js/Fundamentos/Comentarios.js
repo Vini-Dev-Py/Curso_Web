b1a7e108ba3659d2896d64a772ed8766b6feccce
Vinicius Guilherme Batista <vinigbatista21@gmail.com>
2020-04-18 09:37:05 -0300

    Comentatarios no JavaScript

---
+++ ex_js/Fundamentos/Comentarios.js
@@ -0,0 +1,12 @@
+// Comentario de uma linha
+console.log("Comentario 1 !")
+/* 
+comentario de 
+varias linhas
+*/
+console.log("Comentario 2 !")
+/*
+* comentario de 
+* multiplas linhas
+*/
+console.log("Comentario 3 !")
