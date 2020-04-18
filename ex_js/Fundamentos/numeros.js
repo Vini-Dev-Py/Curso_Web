899f01f59a0d23ba0d08191b087701d01de3f049
Vinicius Guilherme Batista <vinigbatista21@gmail.com>
2020-04-18 18:44:00 -0300

    Fundamentos

---
+++ ex_js/Fundamentos/numeros.js
@@ -0,0 +1,12 @@
+const peso1 = 1.0
+const peso2 = Number('2.0')
+
+console.log(peso1, peso2)
+
+const avaliação1 = 9.871
+const avaliação2 = 6.871
+
+const total = avaliação1 + avaliação2
+const media = total / 2
+
+console.log(media)
