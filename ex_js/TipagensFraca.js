7a24794fd7aa6e42f845cd3e08963c9583afb5dc
Vinicius Guilherme Batista <vinigbatista21@gmail.com>
2020-04-18 10:17:13 -0300

    Create TipagensFraca.js

---
+++ ex_js/TipagensFraca.js
@@ -0,0 +1,14 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+
+console.log(typeof(qualquer))
+
+qualquer = 4
+
+console.log(qualquer)
+
+// sem nome generico ou siglas
+
+let valor = ''
+let numero = 1
+let pqp = False // Produto Quimico Perigoso
