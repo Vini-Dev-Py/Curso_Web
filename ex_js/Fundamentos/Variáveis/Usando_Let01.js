27ac0764d03c851b89a453acd178ec3150cec9d4
Vinicius Guilherme Batista <vinigbatista21@gmail.com>
2020-04-25 14:03:11 -0300

    pastas e loop

---
+++ ex_js/Fundamentos/Variáveis/Usando_Let01.js
@@ -0,0 +1,15 @@
+/*var numero = 1 
+{
+    let numero = 2
+    console.log('Dentro =', numero)
+}
+
+console.log('Fora =', numero)
+*/
+let numero = 1 
+{
+    let numero = 2
+    console.log('Dentro =', numero)
+}
+
+console.log('Fora =', numero)
